612ded08e2dc80d2cdc558dfe6053b4507aafe4c
Paul Marceau <paul@themarceaus.net>
2022-12-22 15:07:39 -0500

    Updates to the routes for store and item

--- resources/store.py
+++ resources/store.py
@@ -2,47 +2,47 @@ import uuid
 from flask import *
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import stores
+from schemas import StoreSchema
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+from db import db
+from models import StoreModel
 
 blp = Blueprint("stores", __name__, description="Operations on stores")
 
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
+    @blp.response(200, StoreSchema)
     def get(self, store_id):
-        try:
-            # You presumably would want to include the store's items here too
-            # More on that when we look at databases
-            return stores[store_id]
-        except KeyError:
-            abort(404, message="Store not found.")
+        store = StoreModel.query.get_or_404(store_id)
+        return store
 
     def delete(self, store_id):
-        try:
-            del stores[store_id]
-            return {"message": "Store deleted."}
-        except KeyError:
-            abort(404, message="Store not found.")
+        store = StoreModel.query.get_or_404(store_id)
+        db.session.delete(store)
+        db.session.commit()
+        return {"message": "Store deleted"}, 200
 
 
 @blp.route("/store")
 class StoreList(MethodView):
+    @blp.response(200, StoreSchema(many=True))
     def get(self):
-        return {"stores": list(stores.values())}
+        return StoreModel.query.all()
 
-    def post(self):
-        store_data = request.get_json()
-        if "name" not in store_data:
+    @blp.arguments(StoreSchema)
+    @blp.response(201, StoreSchema)
+    def post(self, store_data):
+        store = StoreModel(**store_data)
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
             abort(
                 400,
-                message="Bad request. Ensure 'name' is included in the JSON payload.",
+                message="A store with that name already exists.",
             )
-        for store in stores.values():
-            if store_data["name"] == store["name"]:
-                abort(400, message=f"Store already exists.")
-
-        store_id = uuid.uuid4().hex
-        store = {**store_data, "id": store_id}
-        stores[store_id] = store
+        except SQLAlchemyError:
+            abort(500, message="An error occurred creating the store.")
 
         return store
--- run.py
+++ run.py
@@ -2,19 +2,30 @@ from flask import Flask
 from flask_smorest import Api
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
+from db import db
+import models
 
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    # app.config["SECRET_KEY"] = 'password'
+    # app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:password@localhost/flask3'
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data1.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    api = Api(app)
 
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config["API_TITLE"] = "Stores REST API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.3"
-app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    with app.app_context():
+        db.create_all()
 
-api = Api(app)
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
 
-api.register_blueprint(ItemBlueprint)
-api.register_blueprint(StoreBlueprint)
+    return app
