fd6c7c759366ea755e28454de6416227da8be7af
Paul Marceau <paul@themarceaus.net>
2022-12-27 22:41:47 -0500

    Added dewscription field to the model

--- models/item.py
+++ models/item.py
@@ -7,7 +7,8 @@ class ItemModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
     price = db.Column(db.Float(precision=2), unique=False, nullable=False)
-
+    description = db.Column(db.String(300))
+    quantity = db.Column(db.Integer)
     store_id = db.Column(db.Integer, db.ForeignKey("stores.id"), unique=False, nullable=False)
     store = db.relationship("StoreModel", back_populates="items")
 
